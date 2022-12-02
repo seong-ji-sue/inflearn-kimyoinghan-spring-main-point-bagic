d2769ae2c14af8b64dc4b18c13d6a2821c3896eb
jisue <tjdwltn121@gmail.com>
2022-12-02 22:20:29 +0900

    12/02 모든 빈 조회

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -21,14 +21,14 @@ public class AllBeanTest {
 
         DiscountService discountService = ac.getBean(DiscountService.class);
         Member member = new Member(1L, "userA", Gradle.VIP);
+        //고정금액 할인 테스트
         int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
 
         Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
         Assertions.assertThat(discountPrice).isEqualTo(1000);
 
+        //퍼센트 금액 할인 테스트
         int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
-
-
         Assertions.assertThat(rateDiscountPrice).isEqualTo(2000);
 
     }
@@ -37,6 +37,7 @@ public class AllBeanTest {
         private final Map<String, DiscountPolicy> policyMap;
         private final List <DiscountPolicy> polices;
 
+        //주입이 일어나려면 DiscountPolicy 가 빈으로 등록되어 있어야 한다.
         @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> polices) {
             this.policyMap = policyMap;
@@ -45,6 +46,7 @@ public class AllBeanTest {
             System.out.println("policyList = " + polices);
         }
 
+        //할인 코드를 넣으면 할인 금액 저장
         public int discount(Member member, int price, String discountCode) {
             DiscountPolicy discountPolicy = policyMap.get(discountCode);
             return discountPolicy.discount(member, price);
