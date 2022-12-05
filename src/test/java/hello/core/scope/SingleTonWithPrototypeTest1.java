965da313b7711d8f035b2bf8d978e134acd5949f
jisue <tjdwltn121@gmail.com>
2022-12-05 17:55:11 +0900

    12/05Provider로 변경

--- src/test/java/hello/core/scope/SingleTonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingleTonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 /**
  * 프로토타입 빈에서 빈여러개 생성시
@@ -38,20 +40,20 @@ public class SingleTonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
     }
 
     //프로토 타입은 빈생성 후 더이상 관리안함 그래서 싱글톤에 주입하면
     //어떤거를 호출하러라도 다 같은 프로토 타입 빈임
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;// 생성 시점에 주입
 
         @Autowired
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
